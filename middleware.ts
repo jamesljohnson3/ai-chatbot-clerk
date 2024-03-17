0cc46d50d6fbb5f5497cb3781ee8fe53cf2e740e
Admin <support@unlimitpotential.com>
2024-03-16 21:48:04 -0400

    j

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
-export { auth as middleware } from './auth'
-
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({});
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
